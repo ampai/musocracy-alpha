785b2f1bcb466c76eb2233e698e790badd2c41b8
Pai <atavatika@gmail.com>
2014-03-23 05:08:31 -0400

    updated config::encryption-key and set main DB conn settings

--- application/config/config.php
+++ application/config/config.php
@@ -224,7 +224,7 @@ $config['cache_path'] = '';
 | MUST set an encryption key.  See the user guide for info.
 |
 */
-$config['encryption_key'] = '';
+$config['encryption_key'] = 'fdmm578bjFui77hv89jj9di9';
 
 /*
 |--------------------------------------------------------------------------
