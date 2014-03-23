a3a47003c896414ac10cbecf5f67159e36f77822
Pai <atavatika@gmail.com>
2014-03-23 03:38:53 -0400

    Updated default route to landing page controller, made new controller for landing page...

---
+++ application/views/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+	    <meta charset="utf-8">
+	    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	    <meta name="viewport" content="width=device-width, initial-scale=1">
+	    <title>Musocracy Player</title>
+
+	    <!-- To fix: Give the CodeIgniter way of finding "base URL" instead of ../../  -->
+		<link href="../../css/main.css"                  rel="stylesheet" type="text/css">
+		<link href="../../css/bootstrap311/bootstrap.min.css" rel="stylesheet" type="text/css">
+
+		<!-- Jquery - Loaded early -->
+		<script src="http://code.jquery.com/jquery.js"></script>
+
+	</head>
+
+	<body>
+		
+
+	
---
+++ application/views/landing_page.php
@@ -0,0 +1,9 @@
+<div class="container">
+	<div class="row">
+		<div class="container-full">
+			<h1>Welcome</h1>
+
+
+		</div>
+	</div>
+</div>
